54d970194b1eb4506dbdaf97e8ca58ae1f2c462a
Himanshu Gulati <64896622+iamhimanshugulati@users.noreply.github.com>
2021-09-13 17:58:51 +0530

    message changed in index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 
 app.use('/users', usersRoutes);
 
-app.get('/', (req, res)=>res.send('Hello from Homepage'));
+app.get('/', (req, res)=>res.send('This is Homepage, kindly refer json file here http://localhost:5000/users'));
 
 
 app.listen(PORT, (req, res)=> console.log(`Server is Running on port http://localhost:${PORT}`));
